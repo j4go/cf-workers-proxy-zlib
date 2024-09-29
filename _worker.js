9eb119a72a2348e35019da8dfbf84779d447dc4c
j4 <yianny@163.com>
2024-09-29 13:45:00 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,199 +1,141 @@
-function logError(request, message) {
-  console.error(
-    `${message}, clientIp: ${request.headers.get(
-      "cf-connecting-ip"
-    )}, user-agent: ${request.headers.get("user-agent")}, url: ${request.url}`
-  );
+// 你要镜像的网站.
+const upstream = 'zh.z-lib.gs'
+
+// 镜像网站的目录，比如你想镜像某个网站的二级目录则填写二级目录的目录名，镜像 google 用不到，默认即可.
+const upstream_path = '/'
+
+// 镜像站是否有手机访问专用网址，没有则填一样的.
+const upstream_mobile = 'zh.1lib.education'
+
+// 屏蔽国家和地区.
+const blocked_region = ['KP', 'SY', 'PK', 'CU']
+
+// 屏蔽 IP 地址.
+const blocked_ip_address = ['0.0.0.0', '127.0.0.1']
+
+// 镜像站是否开启 HTTPS.
+const https = true
+
+// 文本替换.
+const replace_dict = {
+    '$upstream': '$custom_domain',
 }
 
-function createNewRequest(request, url, proxyHostname, originHostname) {
-  const newRequestHeaders = new Headers(request.headers);
-  for (const [key, value] of newRequestHeaders) {
-    if (value.includes(originHostname)) {
-      newRequestHeaders.set(
-        key,
-        value.replace(
-          new RegExp(`(?<!\\.)\\b${originHostname}\\b`, "g"),
-          proxyHostname
-        )
-      );
+// 以下保持默认，不要动
+addEventListener('fetch', event => {
+    event.respondWith(fetchAndApply(event.request));
+})
+
+async function fetchAndApply(request) {
+
+    const region = request.headers.get('cf-ipcountry').toUpperCase();
+    const ip_address = request.headers.get('cf-connecting-ip');
+    const user_agent = request.headers.get('user-agent');
+
+    let response = null;
+    let url = new URL(request.url);
+    let url_hostname = url.hostname;
+
+    if (https == true) {
+        url.protocol = 'https:';
+    } else {
+        url.protocol = 'http:';
     }
-  }
-  return new Request(url.toString(), {
-    method: request.method,
-    headers: newRequestHeaders,
-    body: request.body,
-  });
-}
 
-function setResponseHeaders(
-  originalResponse,
-  proxyHostname,
-  originHostname,
-  DEBUG
-) {
-  const newResponseHeaders = new Headers(originalResponse.headers);
-  for (const [key, value] of newResponseHeaders) {
-    if (value.includes(proxyHostname)) {
-      newResponseHeaders.set(
-        key,
-        value.replace(
-          new RegExp(`(?<!\\.)\\b${proxyHostname}\\b`, "g"),
-          originHostname
-        )
-      );
+    if (await device_status(user_agent)) {
+        var upstream_domain = upstream;
+    } else {
+        var upstream_domain = upstream_mobile;
     }
-  }
-  if (DEBUG) {
-    newResponseHeaders.delete("content-security-policy");
-  }
-  return newResponseHeaders;
-}
 
-/**
- * 替换内容
- * @param originalResponse 响应
- * @param proxyHostname 代理地址 hostname
- * @param pathnameRegex 代理地址路径匹配的正则表达式
- * @param originHostname 替换的字符串
- * @returns {Promise<*>}
- */
-async function replaceResponseText(
-  originalResponse,
-  proxyHostname,
-  pathnameRegex,
-  originHostname
-) {
-  let text = await originalResponse.text();
-  if (pathnameRegex) {
-    pathnameRegex = pathnameRegex.replace(/^\^/, "");
-    return text.replace(
-      new RegExp(`((?<!\\.)\\b${proxyHostname}\\b)(${pathnameRegex})`, "g"),
-      `${originHostname}$2`
-    );
-  } else {
-    return text.replace(
-      new RegExp(`(?<!\\.)\\b${proxyHostname}\\b`, "g"),
-      originHostname
-    );
-  }
-}
-
-async function nginx() {
-  return `<!DOCTYPE html>
-<html>
-<head>
-<title>Welcome to nginx!</title>
-<style>
-html { color-scheme: light dark; }
-body { width: 35em; margin: 0 auto;
-font-family: Tahoma, Verdana, Arial, sans-serif; }
-</style>
-</head>
-<body>
-<h1>Welcome to nginx!</h1>
-<p>If you see this page, the nginx web server is successfully installed and
-working. Further configuration is required.</p>
-
-<p>For online documentation and support please refer to
-<a href="http://nginx.org/">nginx.org</a>.<br/>
-Commercial support is available at
-<a href="http://nginx.com/">nginx.com</a>.</p>
-
-<p><em>Thank you for using nginx.</em></p>
-</body>
-</html>`;
-}
-
-export default {
-  async fetch(request, env, ctx) {
-    try {
-      const {
-        PROXY_HOSTNAME = "zh.z-lib.gs",
-        PROXY_PROTOCOL = "https",
-        PATHNAME_REGEX,
-        UA_WHITELIST_REGEX,
-        UA_BLACKLIST_REGEX,
-        URL302 = "https://zh.z-lib.gs",
-        IP_WHITELIST_REGEX,
-        IP_BLACKLIST_REGEX,
-        REGION_WHITELIST_REGEX,
-        REGION_BLACKLIST_REGEX,
-        DEBUG = false,
-      } = env;
-      const url = new URL(request.url);
-      const originHostname = url.hostname;
-      if (
-        !PROXY_HOSTNAME ||
-        (PATHNAME_REGEX && !new RegExp(PATHNAME_REGEX).test(url.pathname)) ||
-        (UA_WHITELIST_REGEX &&
-          !new RegExp(UA_WHITELIST_REGEX).test(
-            request.headers.get("user-agent").toLowerCase()
-          )) ||
-        (UA_BLACKLIST_REGEX &&
-          new RegExp(UA_BLACKLIST_REGEX).test(
-            request.headers.get("user-agent").toLowerCase()
-          )) ||
-        (IP_WHITELIST_REGEX &&
-          !new RegExp(IP_WHITELIST_REGEX).test(
-            request.headers.get("cf-connecting-ip")
-          )) ||
-        (IP_BLACKLIST_REGEX &&
-          new RegExp(IP_BLACKLIST_REGEX).test(
-            request.headers.get("cf-connecting-ip")
-          )) ||
-        (REGION_WHITELIST_REGEX &&
-          !new RegExp(REGION_WHITELIST_REGEX).test(
-            request.headers.get("cf-ipcountry")
-          )) ||
-        (REGION_BLACKLIST_REGEX &&
-          new RegExp(REGION_BLACKLIST_REGEX).test(
-            request.headers.get("cf-ipcountry")
-          ))
-      ) {
-        logError(request, "Invalid");
-        return URL302
-          ? Response.redirect(URL302, 302)
-          : new Response(await nginx(), {
-              headers: {
-                "Content-Type": "text/html; charset=utf-8",
-              },
-            });
-      }
-      url.host = PROXY_HOSTNAME;
-      url.protocol = PROXY_PROTOCOL;
-      const newRequest = createNewRequest(
-        request,
-        url,
-        PROXY_HOSTNAME,
-        originHostname
-      );
-      const originalResponse = await fetch(newRequest);
-      const newResponseHeaders = setResponseHeaders(
-        originalResponse,
-        PROXY_HOSTNAME,
-        originHostname,
-        DEBUG
-      );
-      const contentType = newResponseHeaders.get("content-type") || "";
-      let body;
-      if (contentType.includes("text/")) {
-        body = await replaceResponseText(
-          originalResponse,
-          PROXY_HOSTNAME,
-          PATHNAME_REGEX,
-          originHostname
-        );
-      } else {
-        body = originalResponse.body;
-      }
-      return new Response(body, {
-        status: originalResponse.status,
-        headers: newResponseHeaders,
-      });
-    } catch (error) {
-      logError(request, `Fetch error: ${error.message}`);
-      return new Response("Internal Server Error", { status: 500 });
+    url.host = upstream_domain;
+    if (url.pathname == '/') {
+        url.pathname = upstream_path;
+    } else {
+        url.pathname = upstream_path + url.pathname;
     }
-  },
-};
+
+    if (blocked_region.includes(region)) {
+        response = new Response('Access denied: WorkersProxy is not available in your region yet.', {
+            status: 403
+        });
+    } else if (blocked_ip_address.includes(ip_address)) {
+        response = new Response('Access denied: Your IP address is blocked by WorkersProxy.', {
+            status: 403
+        });
+    } else {
+        let method = request.method;
+        let request_headers = request.headers;
+        let new_request_headers = new Headers(request_headers);
+
+        new_request_headers.set('Host', url.hostname);
+        new_request_headers.set('Referer', url.hostname);
+
+        let original_response = await fetch(url.href, {
+            method: method,
+            headers: new_request_headers
+        })
+
+        let original_response_clone = original_response.clone();
+        let original_text = null;
+        let response_headers = original_response.headers;
+        let new_response_headers = new Headers(response_headers);
+        let status = original_response.status;
+
+        new_response_headers.set('access-control-allow-origin', '*');
+        new_response_headers.set('access-control-allow-credentials', true);
+        new_response_headers.delete('content-security-policy');
+        new_response_headers.delete('content-security-policy-report-only');
+        new_response_headers.delete('clear-site-data');
+
+        const content_type = new_response_headers.get('content-type');
+        if (content_type.includes('text/html') && content_type.includes('UTF-8')) {
+            original_text = await replace_response_text(original_response_clone, upstream_domain, url_hostname);
+        } else {
+            original_text = original_response_clone.body
+        }
+
+        response = new Response(original_text, {
+            status,
+            headers: new_response_headers
+        })
+    }
+    return response;
+}
+
+async function replace_response_text(response, upstream_domain, host_name) {
+    let text = await response.text()
+
+    var i, j;
+    for (i in replace_dict) {
+        j = replace_dict[i]
+        if (i == '$upstream') {
+            i = upstream_domain
+        } else if (i == '$custom_domain') {
+            i = host_name
+        }
+
+        if (j == '$upstream') {
+            j = upstream_domain
+        } else if (j == '$custom_domain') {
+            j = host_name
+        }
+
+        let re = new RegExp(i, 'g')
+        text = text.replace(re, j);
+    }
+    return text;
+}
+
+
+async function device_status(user_agent_info) {
+    var agents = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];
+    var flag = true;
+    for (var v = 0; v < agents.length; v++) {
+        if (user_agent_info.indexOf(agents[v]) > 0) {
+            flag = false;
+            break;
+        }
+    }
+    return flag;
+}
